903abaa645bd75e913098d6e4aebd70805c23565
veenhouse <luke.veenhuis@gmail.com>
2021-11-09 23:03:45 -0600

    foo

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
     author_email='luke.j.veenhuis@dartmouth.edu',
     description='Simple one hop database lookup service for NCATS Connections Hypothesis Provider',
     packages=       ['chp_look_up',
-                    'chp_look_up.app'],
+                    'chp_look_up.app'
+                    'chp_look_up.migrations'],
     install_requires=REQUIRED_PACKAGES,
     python_requires='>=3.8',
     dependency_links=[
